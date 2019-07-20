5b12a4a1d21e447235fe9d269db4b6c4bb09974c
Zeyad59 <53121720+Zeyad59@users.noreply.github.com>
2019-07-20 20:49:28 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.once('ready', ()  => {
+ console.log('Loading..');
+});
+
+client.on('message', function(message) {
+     if (message.content === "*help") {
+        message.channel.send(`:neutral_face:انت عايز ايه بي الهيلب؟`)
+     }
+});
+
+client.login("NjAyMTU0NDAxNDQzODcyNzk5.XTM6ew.m8cjmRC2Vg0BxQDMeFJ8KdiZr10");
